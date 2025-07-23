f1221edde48ea43e1cead0fc1a24021b37202072
“orodriguez” <omarjavier15@gmail.com>
2025-07-23 18:48:10 -0400

    Prepend

--- Algorithme.Console/Program.cs
+++ Algorithme.Console/Program.cs
@@ -1,4 +1,20 @@
-﻿
-var a = new[] { "a", "b", "c", "d" };
-var i = int.Parse(args[0]);
-Console.WriteLine($"Value at {i} {a[i]}");
+﻿var students = new[]
+{
+    "Arlennys Maria Garcia Reyes",
+    "Augusto Martinez Abreu",
+    "Jeffry Diaz",
+    "Dorley El Roi Tessa",
+    "Oscar Roger Fanfan Nuñez",
+    "Gary Modesto Gomez Maldonado",
+    // "Bryant Tejeda Florimon",
+    // "Carlo Manuel Cabrera Cabral",
+    "Wilnorvel Alexander Silverne",
+    "Luis Antonio Vargas Vizcaino"
+};
+
+var random = new Random();
+var randomIndex = random.Next(0, students.Length);
+
+var chosen = students[randomIndex];
+
+Console.WriteLine($"{chosen} is the chosen!");
---
+++ Algorithme/LnkListNode.cs
@@ -0,0 +1,34 @@
+﻿namespace Algorithme;
+
+public class LnkListNode
+{
+    public int Value { get; private set; }
+
+    public LnkListNode? Next { get; private set; }
+
+    public LnkListNode(int value, LnkListNode? next)
+    {
+        Value = value;
+        Next = next;
+    }
+
+    public void Prepend(int value)
+    {
+        Next = new LnkListNode(Value, null);
+        Value = value;
+    }
+
+    public int[] ToArray()
+    {
+        var result = new List<int>();
+
+        LnkListNode? current = this;
+        while (current != null)
+        {
+            result.Add(current.Value);
+            current = current.Next;
+        }
+
+        return result.ToArray();
+    }
+}
