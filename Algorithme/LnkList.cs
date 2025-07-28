9f23bf7df54b3427b083fb11add9219099490c98
dtfndan <rogerdt2402@hotmail.com>
2025-07-28 18:41:50 -0400

    oscar fanfan bool Contains(T value);  void RemoveFirst(T value);  void RemoveLast(T value);

--- Algorithme/LnkList.cs
+++ Algorithme/LnkList.cs
@@ -1,54 +1,35 @@
-namespace Algorithme;
-
 public class LnkList<T>
 {
     private LnkListNode<T>? _head;
 
-    public LnkList() => 
-        _head = null;
-    
-    // O (1)
+    public LnkList() => _head = null;
+
+    // O(1)
     public void Prepend(T value)
     {
-        // O (1)
-        if (_head == null)
-        {
-            _head = new LnkListNode<T>(value);
-            return;
-        }
-        
-        // O (1)
         _head = new LnkListNode<T>(value, next: _head);
     }
-    
+
     // O(n)
     public void Add(T value)
     {
-        // O(1)
         if (_head == null)
         {
             _head = new LnkListNode<T>(value);
             return;
         }
-        
-        // O(n)
+
         var current = _head;
-        while (current.Next != null) 
+        while (current.Next != null)
             current = current.Next;
 
         current.Next = new LnkListNode<T>(value);
     }
-    
+
     // O(n)
     public void Insert(int index, T value)
     {
-        if (_head == null)
-        {
-            _head = new LnkListNode<T>(value);
-            return;
-        }
-
-        if (index == 0)
+        if (_head == null || index == 0)
         {
             Prepend(value);
             return;
@@ -75,10 +56,10 @@ public class LnkList<T>
     {
         if (_head == null)
             return Array.Empty<T>();
-        
-        var result = new List<T>();
 
+        var result = new List<T>();
         var current = _head;
+
         while (current != null)
         {
             result.Add(current.Value);
@@ -87,38 +68,38 @@ public class LnkList<T>
 
         return result;
     }
-    
+
     // O(n)
     public int Count()
     {
-        if (_head == null)
-            return 0;
-
         var current = _head;
         var count = 0;
+
         while (current != null)
         {
             count++;
             current = current.Next;
         }
+
         return count;
     }
 
+    // O(n) 
     public bool Remove(T value)
     {
         if (_head == null)
             return false;
 
-        if (_head.Value.Equals(value))
+        if (_head.Value!.Equals(value))
         {
-            _head = null;
+            _head = _head.Next;
             return true;
         }
 
         var current = _head;
         while (current.Next != null)
         {
-            if (current.Next.Value.Equals(value))
+            if (current.Next.Value!.Equals(value))
             {
                 current.Next = current.Next.Next;
                 return true;
@@ -132,5 +113,79 @@ public class LnkList<T>
     public IEnumerable<T> ToReversedEnumerable()
     {
         throw new NotImplementedException();
+    }
+
+    // O(n)
+    public bool Contains(T value)
+    {
+        var current = _head;
+        while (current != null)
+        {
+            if (current.Value!.Equals(value))
+                return true;
+            current = current.Next;
+        }
+        return false;
+    }
+
+    // O(n)
+    public void RemoveFirst(T value)
+    {
+        if (_head == null)
+            return;
+
+        if (_head.Value!.Equals(value))
+        {
+            _head = _head.Next;
+            return;
+        }
+
+        var current = _head;
+        while (current.Next != null)
+        {
+            if (current.Next.Value!.Equals(value))
+            {
+                current.Next = current.Next.Next;
+                return;
+            }
+            current = current.Next;
+        }
+    }
+
+    // O(n)
+    public void RemoveLast(T value)
+    {
+        if (_head == null)
+            return;
+
+        LnkListNode<T>? lastMatch = null;
+        LnkListNode<T>? beforeLastMatch = null;
+        LnkListNode<T>? current = _head;
+        LnkListNode<T>? prev = null;
+
+        while (current != null)
+        {
+            if (current.Value!.Equals(value))
+            {
+                lastMatch = current;
+                beforeLastMatch = prev;
+            }
+
+            prev = current;
+            current = current.Next;
+        }
+
+        if (lastMatch == null)
+            return;
+
+        if (beforeLastMatch == null)
+        {
+            // Está en la cabeza
+            _head = _head.Next;
+        }
+        else
+        {
+            beforeLastMatch.Next = lastMatch.Next;
+        }
     }
 }
